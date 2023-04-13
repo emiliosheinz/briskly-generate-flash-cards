86b6523e5ba35bad3c440e0eae106e2584c1c5e8
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-04-13 18:12:21 -0300

    chore: adds forced error

--- src/openai.ts
+++ src/openai.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-  apiKey: '',
+  apiKey: process.env.OPENAI_API_KEY,
 })
 
 export const openai = new OpenAIApi(configuration)
